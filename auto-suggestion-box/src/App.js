4fb8d31b3dddc54c1e4e06f965202260fdc1090a
Samson Adeyinka <samsonadeyinka@hotmail.co.uk>
2024-07-19 02:30:22 +0100

    Install tailwind and add input box

--- auto-suggestion-box/src/App.js
+++ auto-suggestion-box/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="m-6">
+        <label>Product Search</label>
+        <input type="text" id="product-input" class="mr-6 mt-6 bg-gray-50 border rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"></input>
+      </div>
     </div>
   );
 }
